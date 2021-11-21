6a5a3802f0ba2da92ff3df8c3d43dec07c31b82c
John Thompson <john@springframework.guru>
2021-11-21 13:38:35 -0500

    adding stream example

--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.Nullable;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
@@ -14,4 +15,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Nullable
     Book getByTitle(@Nullable String title);
+
+    Stream<Book> findAllByTitleNotNull();
 }
--- src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
+++ src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -24,6 +27,17 @@ public class BookRepositoryTest {
 
     @Autowired
     BookRepository bookRepository;
+
+    @Test
+    void testBookStream() {
+        AtomicInteger count = new AtomicInteger();
+
+        bookRepository.findAllByTitleNotNull().forEach(book -> {
+            count.incrementAndGet();
+        });
+
+        assertThat(count.get()).isGreaterThan(5);
+    }
 
     @Test
     void testEmptyResultException() {
