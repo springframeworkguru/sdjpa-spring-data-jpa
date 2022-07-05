e7732918af443afb14d719dd0cc02720155c5baa
John Thompson <john@springframework.guru>
2022-07-05 14:44:29 -0400

    Updating to Java 17 and Spring Boot 3.x

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -3,8 +3,8 @@ package guru.springframework.jdbc.dao;
 import guru.springframework.jdbc.domain.Author;
 import guru.springframework.jdbc.repositories.AuthorRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 /**
  * Created by jt on 8/28/21.
